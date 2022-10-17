b85cc21dba8a7a9ac38f90ac9a53ea82bf950543
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-10-17 09:47:42 -0300

    feat models

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class SexChoices(models.TextChoices):
+    Macho = "Macho"
+    Femea = "Femea"
+    Default = "Não informado"
+
+class Animal(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(max_length=15, choices=SexChoices.choices, default=SexChoices.Default)
+    group = models.ForeignKey("groups.Group", on_delete=models.CASCADE, related_name="animals")
+    traits = models.ManyToManyField("traits.Trait", related_name="animals")
+
