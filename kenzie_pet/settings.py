f9ab16bceb95d2b9d4c8c775145b043db08c25d6
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-10-17 09:04:28 -0300

    fix: installed_apps

--- kenzie_pet/settings.py
+++ kenzie_pet/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'animals',
+    'groups',
+    'traits',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
