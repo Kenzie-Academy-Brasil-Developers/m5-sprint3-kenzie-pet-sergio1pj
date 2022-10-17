0124c13cece1346805bac5d8ed8bc55db6c82dc6
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-10-17 11:42:21 -0300

    fix models

--- animals/models.py
+++ animals/models.py
@@ -12,4 +12,3 @@ class Animal(models.Model):
     sex = models.CharField(max_length=15, choices=SexChoices.choices, default=SexChoices.Default)
     group = models.ForeignKey("groups.Group", on_delete=models.CASCADE, related_name="animals")
     traits = models.ManyToManyField("traits.Trait", related_name="animals")
-
